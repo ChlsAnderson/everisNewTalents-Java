7c0ab3d6abf8aa50451b36751e67086706350a94
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-28 20:05:28 -0300

    implementa os métodos getNo e get

--- src/main/java/com/listacircular/ListaCircular.java
+++ src/main/java/com/listacircular/ListaCircular.java
@@ -3,8 +3,27 @@ package com.listacircular;
 public class ListaCircular<T> {
 
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+        if(this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vázia");
+
+        if(index==0){
+            return this.cauda;
+        }
+
+        No<T> noAuxiliar = this.cauda;
+        for(int i=0;(i<index)&&(noAuxiliar!=null);i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
 
     public boolean isEmpty(){
         return this.tamanhoLista == 0?true:false;

dca1675b2c124df9156fc31db8b04adae47b15c0
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-28 18:44:17 -0300

    Implementa método Add

--- src/main/java/com/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/main/java/com/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -21,6 +21,20 @@ public class ListaDuplamenteEncadeada<T> {
         return this.getNo(index).getConteudo();
     }
 
+    public void add(T elemento){
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if(primeiroNo ==null){
+            primeiroNo = novoNo;
+        }
+        if(ultimoNo !=null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
 
@@ -32,7 +46,7 @@ public class ListaDuplamenteEncadeada<T> {
     }
 
     public int size(){
-        return tamanhoLista;
+        return this.tamanhoLista;
     }
 
 }

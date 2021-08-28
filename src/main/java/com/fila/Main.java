bfdf895718c48e14a896cc02f9f76a2585e60e67
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-28 14:41:58 -0300

    Fila e a utilização dela implementada só com enqueue

---
+++ src/main/java/com/fila/Main.java
@@ -0,0 +1,18 @@
+package com.fila;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue(new No("primeiro"));
+        minhaFila.enqueue(new No("segundo"));
+        minhaFila.enqueue(new No("terceiro"));
+        minhaFila.enqueue(new No("quarto"));
+
+        System.out.println(minhaFila);
+
+        
+    }
+}

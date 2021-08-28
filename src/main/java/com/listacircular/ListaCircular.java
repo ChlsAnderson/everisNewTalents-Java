143432705c6abefaef189672e9aca94d9967e36f
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-28 20:52:04 -0300

    implementa o método toString

--- src/main/java/com/listacircular/ListaCircular.java
+++ src/main/java/com/listacircular/ListaCircular.java
@@ -71,4 +71,16 @@ public class ListaCircular<T> {
         return this.tamanhoLista;
     }
 
+    @Override
+    public String toString() {
+        String strRetorno = "";
+
+        No<T> noAuxiliar = this.cauda;
+        for(int i=0;i<this.size();i++){
+            strRetorno += "[No{Conteudo="+noAuxiliar.getConteudo()+"}]---->";
+            noAuxiliar=noAuxiliar.getNoProximo();
+        }
+        strRetorno += this.size()!= 0 ? "(Retorna ao inicio)" :"null";
+        return strRetorno;
+    }
 }

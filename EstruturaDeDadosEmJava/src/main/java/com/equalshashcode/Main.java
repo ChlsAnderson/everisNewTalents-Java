69877844cdb92252547e24e21e19b6c28afaa1f2
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-29 15:49:41 -0300

    Implementação usando Equals e Hashcode

---
+++ EstruturaDeDadosEmJava/src/main/java/com/equalshashcode/Main.java
@@ -0,0 +1,24 @@
+package com.equalshashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("Ford"));
+        listaCarros.add(new Carro("Chevrolet"));
+        listaCarros.add(new Carro("Volkswagen"));
+
+        System.out.println(listaCarros.contains(new Carro("Ford")));
+        System.out.println(new Carro("Ford").hashCode());
+        System.out.println(new Carro("Ford1").hashCode());
+
+        Carro carro1 = new Carro("ford");
+        Carro carro2 = new Carro("Chevrolet");
+
+        System.out.println(carro1.equals(carro2));
+    }
+}

c202376ab0de5c7647465cefab02a683b0df3bd5
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-29 00:27:41 -0300

    Implementa método remover

--- EstruturaDeDadosEmJava/src/main/java/com/arvorebinaria/ArvoreBinaria.java
+++ EstruturaDeDadosEmJava/src/main/java/com/arvorebinaria/ArvoreBinaria.java
@@ -62,4 +62,30 @@ public class ArvoreBinaria<T extends Comparable<T>> {
             exibirInOrdem(atual.getNoDir());
         }
     }
+
+    public void remover(T conteudo){
+        try{
+            BinNo<T> atual = this.raiz;
+            BinNo<T> pai = null;
+            BinNo<T> filho = null;
+            BinNo<T> temp = null;
+
+            while (atual != null && !atual.getConteudo().equals(conteudo)){
+                pai =atual;
+                if(conteudo.compareTo(atual.getConteudo())<0){
+                    atual = atual.getNoEsq();
+                }else {
+                    atual = atual.getNoDir();
+                }
+            }
+
+            if(atual==null){
+                System.out.println("Conteudo nao encontrado. Bloco Try");
+            }
+
+
+        }catch(NullPointerException erro){
+            System.out.println("Conteudo nao encontrado. Bloco Catch");
+        }
+    }
 }

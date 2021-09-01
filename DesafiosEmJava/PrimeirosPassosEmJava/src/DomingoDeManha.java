43b1308f4b9a9711cf55cb01f04eb2b62896b747
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-08-31 21:34:13 -0300

    Desafio 1 em andamento

--- DesafiosEmJava/PrimeirosPassosEmJava/src/DomingoDeManha.java
+++ DesafiosEmJava/PrimeirosPassosEmJava/src/DomingoDeManha.java
@@ -6,17 +6,23 @@ public class DomingoDeManha {
         public static void main(String[] args) throws IOException {
             Scanner leitor = new Scanner(System.in);
             String[] relogio = leitor.nextLine().split(":");    		
-            // while (      ) {
             int hora = Integer.parseInt(relogio[0]);
             int min = Integer.parseInt(relogio[1]);
-            //     switch (      ) {
-            //         case(7): System.out.println("Atraso maximo: " + min); break;
-            //         case(8): System.out.println("Atraso maximo: " + (          ); break;
-            //         case(9): System.out.println("Atraso maximo: " + (          ); break;
-            //         default: System.out.println("Atraso maximo: 0");
-            //     }
-            // }
-        System.out.println(hora + " : " + min + " alguma coisa");
+            while ( 5 <= hora & hora <= 9  ) {
+                while( 0 <= min & min <= 59){
+                switch ( hora ) 
+                {
+                    case (7): System.out.println("Atraso maximo: " + min); break;
+                    case (8): System.out.println("Atraso maximo: " +   (min  +60)    ); break;
+                    case (9): System.out.println("Atraso maximo: " +    (min  +120)     ); break;
+                    default: System.out.println("Atraso maximo: 0"); break;
+                }
+                min++;
+                }
+                System.out.println(hora  +" : " + (min+60) + " alguma coisa");
+                min = 0;
+                hora++;
+            }
         }
         
     // }

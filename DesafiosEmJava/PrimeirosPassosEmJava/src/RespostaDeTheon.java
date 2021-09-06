f6df18105f9f9ce643b002d0dc7a6ded064d19ab
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-06 19:55:14 -0300

    Codigo com as saídas e passou o desafio mas não é as mesmas entradas do enunciado

--- DesafiosEmJava/PrimeirosPassosEmJava/src/RespostaDeTheon.java
+++ DesafiosEmJava/PrimeirosPassosEmJava/src/RespostaDeTheon.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 // public class {
 public class RespostaDeTheon {
     public static void main(String[] args) throws IOException {
-		Scanner leitor = new Scanner(System.in);
-		int N = leitor.nextInt();
-		int T, menor = Integer.MAX_VALUE, posMenor = 0;
-		for (int i=0;i<N;++i        ) {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int T, menor = Integer.MAX_VALUE, posMenor = 0;
+        
+        for (int i = 0; i < N; ++i) {
             T = leitor.nextInt();
-            if ( i==1   ) {
-			} else if (  T<menor  ) {
-				posMenor  =i;
-                menor = i+1;
-			}
-		}
-		System.out.println(posMenor);
-        leitor.close();
+            if (T < menor) {
+                menor = T;
+                posMenor = i+1;
+            }
+        }
+          System.out.println(posMenor);
     }
 	
 }

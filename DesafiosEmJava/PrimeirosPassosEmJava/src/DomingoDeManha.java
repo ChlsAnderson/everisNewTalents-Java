61b6562dfba69a004a5dde3694aeb258bc6943f8
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-06 14:33:53 -0300

    Implementado o desafio Domingo de manhã com o método hasNext()

--- DesafiosEmJava/PrimeirosPassosEmJava/src/DomingoDeManha.java
+++ DesafiosEmJava/PrimeirosPassosEmJava/src/DomingoDeManha.java
@@ -5,26 +5,18 @@ public class DomingoDeManha {
   // public class Desafio {      
         public static void main(String[] args) throws IOException {
             Scanner leitor = new Scanner(System.in);
-            String[] relogio = leitor.nextLine().split(":");    		
-            int hora = Integer.parseInt(relogio[0]);
-            int min = Integer.parseInt(relogio[1]);
-            while ( 5 <= hora & hora <= 9  ) {
-                while( 0 <= min & min <= 59){
+            while ( leitor.hasNext() ) {
+                String[] relogio = leitor.nextLine().split(":");    		
+                int hora = Integer.parseInt(relogio[0]);
+                int min = Integer.parseInt(relogio[1]);
                 switch ( hora ) 
                 {
                     case (7): System.out.println("Atraso maximo: " + min); break;
                     case (8): System.out.println("Atraso maximo: " +   (min  +60)    ); break;
                     case (9): System.out.println("Atraso maximo: " +    (min  +120)     ); break;
-                    default: System.out.println("Atraso maximo: 0"); break;
+                    default: System.out.println("Atraso maximo: 0");
                 }
-                min++;
-                }
-                System.out.println(hora  +" : " + (min+60) + " alguma coisa");
-                min = 0;
-                hora++;
             }
         }
-        
-    // }
-    
+
 }

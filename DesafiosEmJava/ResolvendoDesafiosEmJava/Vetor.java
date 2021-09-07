46ea80bc48f06274c35115109a300b77fdcf42ba
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 19:40:56 -0300

    Vetor

---
+++ DesafiosEmJava/ResolvendoDesafiosEmJava/Vetor.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Vetor{
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        double X = leitor.nextDouble();
+        double[] N = new double[100];
+        N[0] = X;
+        for (                                       ) {
+        	
+        }
+        for (                              ) {
+        	System.out.println(String.format("N[" + i + "] = %.4f", N[i]));
+        }
+    }
+	
+}

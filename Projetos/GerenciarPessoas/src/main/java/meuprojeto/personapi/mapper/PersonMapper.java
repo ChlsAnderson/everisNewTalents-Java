56d60653d2ddf6d7640a8442ccbc43c0b64200be
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 14:07:15 -0300

    Commit do projeto api person

---
+++ Projetos/GerenciarPessoas/src/main/java/meuprojeto/personapi/mapper/PersonMapper.java
@@ -0,0 +1,18 @@
+package meuprojeto.personapi.mapper;
+
+import meuprojeto.personapi.dto.request.PersonDTO;
+import meuprojeto.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}

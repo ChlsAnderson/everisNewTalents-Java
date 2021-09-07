08276c7e554e546cac69c94d51ae6c3a2f25fbfb
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 20:12:57 -0300

    Uniforme resolvido

--- DesafiosEmJava/ResolvendoDesafiosEmJava/Uniforme.java
+++ DesafiosEmJava/ResolvendoDesafiosEmJava/Uniforme.java
@@ -1,107 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
-
-public class Uniforme{
-
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static PrintWriter out = new PrintWriter(System.out);
-
+public class Uniforme {
     public static void main(String[] args) throws IOException {
-        String owner, l, color, size;
-        String[] details;
-        Tshirt[] tshirts;
-        Tshirt tshirt;
-        int N;
-        boolean firstCase = true;
-        while (!(l = read()).equals("0")) {
-           
-            } else {
-                out.println();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int linhas = Integer.parseInt(st.nextToken());
+        List<Uniforme> uniformes = new ArrayList<>();
+        
+        for (int i = 0; i <= linhas; i++) {
+            var next = br.readLine();
+            if(next.equals("0")) {
+                break;
             }
-            N = toInt(l);
-            tshirts = new Tshirt[N];
-            for (int i = 0; i < N; i++) {
-               
-            }
-            Arrays.sort(tshirts, new Comparator<Tshirt>() {
-                @Override
-                public int compare(Tshirt t1, Tshirt t2) {
-                    String color1 = t1.getColor();
-                    String color2 = t2.getColor();
-                    int diff = color1.compareTo(color2);
-                    if (diff != 0) {
-                        return diff;
-                    } else {
-                       
-                        }
-                        if (                           ) {
-                            return -1;
-                        }
-                        return 1;
-                    }
-                }
-            });
-            for (                                  ) {
-    
-            }
-
+            Uniforme uniforme = new Uniforme();
+            uniforme.setNomeEstudante(next);
+            var camiseta = br.readLine().split(" ");
+            uniforme.setCorUniforme(camiseta[0]);
+            uniforme.setTamUniforme(camiseta[1]);
+            uniformes.add(uniforme);
         }
-        out.close();
+        Comparator<Uniforme> comparator = Comparator
+                .comparing(Uniforme::getCorUniforme).reversed()
+                .thenComparing(Uniforme::getTamUniforme).reversed()
+                .thenComparing(Uniforme::getNomeEstudante);
+        
+        uniformes.stream().sorted(comparator).forEach(System.out::println);
     }
-
-    private static String read() throws IOException {
-        return in.readLine();
-    }
-
-    private static int toInt(String s) {
-        return Integer.parseInt(s);
-    }
-}
-
-class Tshirt {
-
-    private String size;
-    private String color;
-    private String owner;
-
-    public Tshirt(String size, String color, String owner) {
-        this.size = size;
-        this.color = color;
-        this.owner = owner;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    @Override
-    public String toString() {
-        return this.getColor() + " " + this.getSize() + " " + this.getOwner();
+    
+    public static class Uniforme {
+        private String nomeEstudante;
+        private String corUniforme;
+        private String tamUniforme;
+        
+        @Override
+        public String toString() {
+            return corUniforme + " " + tamUniforme + " "+ nomeEstudante;
+        }
+        
+        public String getNomeEstudante() {
+            return nomeEstudante;
+        }
+        
+        public void setNomeEstudante(String nomeEstudante) {
+            this.nomeEstudante = nomeEstudante;
+        }
+        
+        public String getCorUniforme() {
+            return corUniforme;
+        }
+        
+        public void setCorUniforme(String corUniforme) {
+            this.corUniforme = corUniforme;
+        }
+        
+        public String getTamUniforme() {
+            return tamUniforme;
+        }
+        
+        public void setTamUniforme(String tamUniforme) {
+            this.tamUniforme = tamUniforme;
+        }
     }
 }
 

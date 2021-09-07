a7fcca42a3f55a02979c133415e1118d1c806f25
charles_mesquita <charles_mesquita@kawasakibrasil.com>
2021-09-07 20:15:26 -0300

    Correção do nome da classe

--- DesafiosEmJava/ResolvendoDesafiosEmJava/Uniformes.java
+++ DesafiosEmJava/ResolvendoDesafiosEmJava/Uniformes.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Uniforme {
+public class Uniformes {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
